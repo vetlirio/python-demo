2cd2d3efdfecd107320e416ba646b87723e39593
vetlirio <vetle.pt@gmail.com>
2025-04-01 17:16:01 +0200

    first commit

---
+++ demo.py
@@ -0,0 +1,9 @@
+import time
+def seconds_counter():
+    seconds_passed = 0
+    while True:
+        print(f"Seconds passed: {seconds_passed}")
+        time.sleep(3)
+        seconds_passed += 3
+
+seconds_counter()
---
+++ script.py
@@ -0,0 +1,13 @@
+matte = 1 + 1 +16
+print(matte)
+
+
+import time
+def seconds_counter():
+    seconds_passed = 0
+    while True:
+        print(f"Seconds passed: {seconds_passed}")
+        time.sleep(3)
+        seconds_passed += 3
+
+seconds_counter()
