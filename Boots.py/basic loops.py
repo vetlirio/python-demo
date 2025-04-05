0ce08475f710c020633fe371969aaea7ff7defde
vetlirio <vetle.pt@gmail.com>
2025-04-05 22:26:04 +0200

    more loops

--- Boots.py/basic loops.py
+++ Boots.py/basic loops.py
@@ -25,4 +25,24 @@ def simple(end):
 
 print(simple(21))
 
-    
+
+# more loops 
+
+# print 1-200 with 25 increments
+
+for i in range(1, 201, 30):
+    print(i)
+
+
+# create a loop that counts down from 1.000.000 to -1.000.000 in increments of 250.000
+
+for i in range(1000000, -1250000, -250000):
+    print(i)
+
+# same thing, but make it a function
+
+def counting_down(start, end, step):
+    for i in range(start, end, step):
+        print(i)
+
+counting_down(1000000, -1250000, -250000)
