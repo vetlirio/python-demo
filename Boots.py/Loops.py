e92aa06a1b9395b121a45a7ce7945f54de82b94f
vetlirio <vetle.pt@gmail.com>
2025-04-05 20:42:17 +0200

    loops example and timer

--- Boots.py/Loops.py
+++ Boots.py/Loops.py
@@ -3,3 +3,32 @@ def print_numbers():
         print(i)
 print(print_numbers())
 
+"""Match Countdown
+
+We need a timer to countdown the start of PvP matches in Fantasy Quest.
+Assignment
+
+In the countdown_to_start function, write a loop that counts down from 10 to 1. At each iteration, print the number with an ellipsis:
+
+i...
+
+However, when i is 1, it should print 1...Fight! instead.
+"""
+
+def countdown_to_start():
+    for i in range (10, 1, -1):
+        print (f"{i}...")
+    print("1...Fight!")
+
+
+def test():
+    print("Counting down to match start:")
+    countdown_to_start()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
