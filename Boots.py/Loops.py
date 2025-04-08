f2714dd87dc3a8da497c5f62340ea3cfe2dc1d3d
vetlirio <vetle.pt@gmail.com>
2025-04-08 03:34:41 +0200

    for/if

--- Boots.py/Loops.py
+++ Boots.py/Loops.py
@@ -32,3 +32,29 @@ def main():
 
 
 main()
+
+
+def contains_leather_scraps(items):
+    found = False
+    for item in items:
+        if item == "Leather Scraps":
+            found = True
+
+def check_character_levels():
+    old_character_levels = [1, 42, 43, 53, 12, 3, 32, 34, 54, 32, 43]
+    new_character_levels = [1, 42, 45, 54, 12, 3, 32, 38, 54, 32, 42]
+
+    # don't touch above this line
+
+    for i in range(0, len(old_character_levels)):
+        if new_character_levels[i] > old_character_levels[i]:
+            print(i)
+            
+            
+def find_max(nums):
+    max_so_far = float("-inf")
+    for num in nums:
+        if num > max_so_far:
+            max_so_far = num
+    return max_so_far
+    
